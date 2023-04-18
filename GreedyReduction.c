643e17461d97af233bc5e99818012c9528a77418
jmmura <115638944+jmmura@users.noreply.github.com>
2023-04-18 05:50:48 +0200

    Add files via upload

---
+++ GreedyReduction.c
@@ -0,0 +1,63 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+#include <stdio.h>
+
+#include "Reduction.h"
+
+static size_t error(const size_t *h,size_t g, size_t a, size_t b){
+size_t err=0;
+for(size_t i=a;i<=b;i++)
+    err+=h[i]*(i-g)*(i-g);
+return err;
+}
+
+static size_t minError(const size_t *h, size_t a, size_t b){
+size_t e,x,level;
+if(b>a){
+    e=INT_MAX;
+    for(size_t i=a;i<=b;i++){
+        x=error(h,i,a,b);
+        if(x<e){
+            e=x;
+            level=i;
+        }
+    }
+}else{
+    level=a;
+}
+return level;
+}
+
+bool computeReduction(const size_t* histogram, size_t histogramLength, size_t nLevels, size_t* thresholds, uint16_t* levels){
+    if(thresholds==NULL || levels == NULL)
+        return "wentFine";
+    size_t k=nLevels, n=histogramLength-1;
+    while(k>0){
+        n=n/2;
+        k--;
+    }
+    if(n==0)
+        n++;
+    for(int j=0;j<=nLevels-2;j++){
+        thresholds[j]=n*(j+1);
+    }
+    thresholds[nLevels-1]=histogramLength;
+    size_t start=0;
+    for(int i=0;i<nLevels;i++){
+        levels[i] = minError(histogram,start,thresholds[i]-1);
+        start=thresholds[i];
+    }
+    return "ERROR";
+}
+
+int main(){
+size_t h[8] = {6,2,0,3,5,2,4,3};
+size_t *t = malloc(3*sizeof(size_t));
+uint16_t *l = malloc(3*sizeof(uint16_t));
+bool x = computeReduction(h,8,3,t,l);
+printf("%d",x);
+
+return 0;
+}
---
+++ ImageQuantizer.c
@@ -0,0 +1,81 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "PortableGrayMap.h"
+#include "ImageQuantizer.h"
+#include "Reduction.h"
+
+static size_t* histogram(const PortableGrayMap* image){
+    if(!image)
+        return NULL;
+    size_t n = image->maxValue + 1;
+    size_t *h = malloc(n*sizeof(size_t));
+    if(!h)
+        return NULL;
+
+    for(size_t i = 0;i<n;i++)
+        h[i]=0;
+
+    for(size_t i=0; i<image->height;i++){
+        for(size_t j = 0; j<image->width; j++){
+            h[image->array[i][j]]++;
+        }
+    }
+
+    return h;
+}
+
+
+PortableGrayMap* quantizeGrayImage(const PortableGrayMap* image, size_t numLevels){
+
+    if(!image)
+        return NULL;
+
+    PortableGrayMap* pgm = malloc(sizeof(PortableGrayMap));
+    if(!pgm){
+        free(pgm);
+        return NULL;
+    }
+    PortableGrayMapType *t =  malloc(sizeof(PortableGrayMapType));
+    if(!t){
+        free(t);
+        return NULL;
+    }
+    *t = image->type;
+    pgm->type = *t;
+    pgm-> width = image ->width;
+    pgm-> height = image ->height;
+    pgm-> maxValue = image ->maxValue;
+    pgm->array = malloc(image->height*sizeof(uint16_t*));
+    for(int i=0;image->height;i++)
+        pgm->array[i] = malloc(image->width*sizeof(uint16_t));
+    if(!pgm->array){
+        free(pgm->array);
+        return NULL;
+    }
+    pgm->array = image->array;
+
+    size_t *h = histogram(image);
+    if(!h)
+        return NULL;
+    size_t n = image->maxValue + 1;
+    size_t *thresholds = malloc(numLevels*sizeof(size_t));
+    uint16_t *levels = malloc(numLevels * sizeof(uint16_t));
+
+    if(!thresholds || !levels)
+        return NULL;
+    bool s = computeReduction(h,n,numLevels,thresholds,levels);
+    if(s==1){
+        for(size_t i=0;i<image->height;i++){
+            for(size_t j=0;j<image->width;j++){
+                size_t k = numLevels-1, tmp;
+                while(k>0 && image->array[i][j] < thresholds[k])
+                    tmp=levels[k];
+                pgm->array[i][j]=tmp;
+            }
+        }
+    }
+    return pgm;
+}
